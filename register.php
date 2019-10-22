e2576b4ef4d52dc6c7389b979b82df725218412c
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-10-22 14:45:06 +0200

    checks if register data in db, if not, adds to db

--- register.php
+++ register.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <?php
-// include ("config/setup.php");
-// require "config/setup.php";
+include ("includes/connect.php");
 ini_set("display_errors", true);
-// session_start();
-    // include("functions/functions.php");
+session_start();
 ?>
 <html lang="en" dir="ltr">
 	<head>
@@ -19,7 +17,7 @@ ini_set("display_errors", true);
 				</a>
 			</div>
 		<div class="main_wrapper">
-			<!--Navigation bar-->
+<!--Navigation bar-->
 			<div class="menubar">
 				<ul id="menu">
 						<li><a href="index.php">Home</a></li>
@@ -33,13 +31,13 @@ ini_set("display_errors", true);
 						</div>
 				</div>
 				<script>
-				/* When the user clicks on the button,
-				toggle between hiding and showing the dropdown content */
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
 				function myFunction() {
 				document.getElementById("myDropdown").classList.toggle("show");
 				}
 
-				// Close the dropdown if the user clicks outside of it
+// Close the dropdown if the user clicks outside of it
 				window.onclick = function(event) {
 					if (!event.target.matches('.dropbtn')) {
 						var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -54,66 +52,80 @@ ini_set("display_errors", true);
 				}
 				</script>
 			</div>
-			<!--content wrapper starts-->
+<!--content wrapper starts-->
 			<div class="content_wrapper">
 				<form action="register.php" method="POST" enctype="multipart/form-data">
-				<table align="center">
-					<tr align="center">
-						<td colspan="6" style="padding:15px; color:white;"><h2>Create your Account</h2></td>
-					</tr>
-					<tr>
-						<td align="right" style="padding:15px; color:white;">Name: </td>
-						<td><input type="text" name="u_name" required/></td>
-					</tr>
-					<tr>
-						<td align="right" style="padding:15px; color:white;">E-mail: </td>
-						<td><input type="text" name="u_email" required/></td>
-					</tr>
-					<tr>
-						<td align="right" style="padding:15px; color:white;">Password: </td>
-						<td><input type="password" name="u_passwd" required/></td>
-					</tr>
-					<tr>
-						<td align="right" style="padding:15px; color:white;">Image: </td>
-						<td><input type="file" name="u_image" required/></td>
-					</tr>
-					<tr>
-						<td align="right" style="padding:15px; color:white;">Contact Number: </td>
-						<td><input type="text" name="u_contact" required/></td>
-					</tr>
-					<tr align="center" >
-						<td colspan="6" style="padding:15px"><input type="submit" name="register" value="Create Account"/></td>
-					</tr>
-				</table>
-			</form>
-		</div>
+					<table align="center">
+						<tr align="center">
+							<td colspan="6" style="padding:15px; color:white;"><h2>Create your Account</h2></td>
+						</tr>
+						<tr>
+							<td align="right" style="padding:15px; color:white;">Name: </td>
+							<td><input type="text" name="u_name" required/></td>
+						</tr>
+						<tr>
+							<td align="right" style="padding:15px; color:white;">E-mail: </td>
+							<td><input type="text" name="u_email" required/></td>
+						</tr>
+						<tr>
+							<td align="right" style="padding:15px; color:white;">Password: </td>
+							<td><input type="password" name="u_passwd" required/></td>
+						</tr>
+						<tr>
+							<td align="right" style="padding:15px; color:white;">Image: </td>
+							<td><input type="file" name="u_image" required/></td>
+						</tr>
+						<tr>
+							<td align="right" style="padding:15px; color:white;">Contact Number: </td>
+							<td><input type="text" name="u_contact" required/></td>
+						</tr>
+						<tr align="center" >
+							<td colspan="6" style="padding:15px"><input type="submit" name="register" value="Register"/></td>
+						</tr>
+					</table>
+				</form>
 			</div>
-			<!--content wrapper ends-->
-			<!--footer starts-->
+<!--content wrapper ends-->
+<!--footer starts-->
 			<div id="footer">
 				<h2 style="text-align:center; padding-top:30px;">rengelbr</h2>
 			</div>
 		</div>
-		<!--footer ends-->
+<!--footer ends-->
 	</body>
 </html>
 <?php
-	$con = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "qwerqwer");
-	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	// if ($con)
-	// 	echo "yea boi\n";
-	// print_r($_POST);
-	echo $u_name = $_POST['u_name']."\n";
-	echo $u_email = $_POST['u_email']."\n";
-	echo $u_passwd = hash('whirlpool', $_POST['u_passwd'])."\n";
-	$u_image = $_FILES['u_image'];
-	echo $u_contact = $_POST['u_contact']."\n";
+//check if register button clicked
+	if (isset($_POST['register'])) {
+//get registration data
+		$u_name = $_POST['u_name'];
+		$u_email = $_POST['u_email'];
+		$u_passwd = hash('whirlpool', $_POST['u_passwd']);
+		$u_image = $_FILES['u_image']['name'];
+		$u_image_tmp = $_FILES['u_image']['tmp_name'];
+		move_uploaded_file($u_image_tmp, "client/client_images/$u_image");
+		$u_contact = $_POST['u_contact'];
+//check if user email exists in db
+		$check = $con->prepare("SELECT * FROM users WHERE user_email=?");
+		$check->execute([$u_email]);
+		$ret = $check->fetch();
+		if ($ret) {
+			echo "<script>window.alert('This user exists!')</script>";
+		}
+		else {
+//execute insert query
+			$sql = "INSERT INTO users (`user_name`, `user_passwd`, `user_email`, `user_contact`, `user_image`) VALUES ('$u_name', '$u_passwd', '$u_email', '$u_contact', '$u_image')";
+			$con->exec($sql);
+//save session vars for later use
+			$check = $con->prepare("SELECT `user_id` FROM users WHERE user_email=?");
+			$check->execute([$u_email]);
+			$ret = $check->fetch();
+			$_SESSION['user_email'] = $u_email;
+			$_SESSION['user_id'];
+			echo "<script>window.open('client/my_account.php', '_self')</script>";
+		}
+	}
 
-	$sql = "INSERT INTO users (`user_name`, `user_passwd`, `user_email`, `user_contact`, `user_image`) VALUES ($u_name, $u_passwd, $u_email, $u_contact, $u_image);";
-	$con->exec($sql);
-	// $stmt = $dbh->prepare($sql);
-	// $stmt->exec(['u_name'=>$u_name, 'u_passwd'=>$u_passwd, 'u_email'=>$u_email, 'u_contact'=>$u_contact, 'u_image'=>$u_image]);
-	echo"post added?"
 ?>
 
 
