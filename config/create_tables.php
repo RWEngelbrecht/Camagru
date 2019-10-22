29d7f9b2ad261053c908af685b1a4a8ff5673e13
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-10-22 14:41:55 +0200

    can now drop instance of db and remake it

--- config/create_tables.php
+++ config/create_tables.php
@@ -8,9 +8,9 @@ try {
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	$sql = "CREATE TABLE IF NOT EXISTS $table(
-	`ID` INT(100) AUTO_INCREMENT PRIMARY KEY,
+	`user_id` INT(100) AUTO_INCREMENT PRIMARY KEY,
 	`user_name` varchar(255) NOT NULL,
-	`user_passwd` varchar(100) NOT NULL,
+	`user_passwd` varchar(255) NOT NULL,
 	`user_email` varchar(255) NOT NULL,
 	`user_contact` varchar(100) NOT NULL,
 	`user_image` varchar(255))";
--- config/setup.php
+++ config/setup.php
@@ -3,13 +3,10 @@
 include 'config/create_db.php';
 include 'config/create_tables.php';
 // include './create_tables.php';
-
 // $dbh = create_db("db_camagru");
 
 
 // echo "dbhost = ".$db_host;
-// $sql = "DROP DATABASE IF EXISTS db_camagru";
-// $dbh->exec($sql);
 
 
 ?>
