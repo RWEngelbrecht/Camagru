cc40d3d1aaf85a174461cadb4074b82318b7180e
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-10-23 13:13:27 +0200

    made boolean function for error checks

--- functions/functions.php
+++ functions/functions.php
@@ -2,12 +2,12 @@
 include ('../includes/connect.php');
 function verif_email($u_email, $ver_code) {
 	$subject = "Activate your account with Camagru.";
-	$body = "Please click <a href='http://localhost:8080/Camagru/verify_email.php?ver_key=".$ver_code."'>here</a> to activate your account.";
-	$headers = "From: rigardte@gmail.com\r\n";
+	$body = "Please click <a href='http://localhost:8080/Camagru/client/verify_email.php?ver_key=".$ver_code."'>here</a> to activate your account.";
+	$headers = "From: camagru.rigardt@gmail.com\r\n";
 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 	if (mail($u_email,$subject,$body,$headers))
-		echo "<h2>Verfication e-mail sent!</h2>";
+		return true;
 	else
-		echo "<h2>Something went wrong! Could not send verification e-mail.</h2>";
+		return false;
 }
 ?>
