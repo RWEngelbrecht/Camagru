2daab716f0a4a0e6bd9fa5728b5f77ba18a25f7c
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-10-23 13:12:29 +0200

    cleaned up files

--- config/create_db.php
+++ config/create_db.php
@@ -1,7 +1,6 @@
 <?php
 include 'config/database.php';
 
-
 try {
 	$dbh = new PDO($dsn, $db_user, $db_passwd);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- config/setup.php
+++ config/setup.php
@@ -1,12 +1,4 @@
 <?php
-// include './database.php';
 include 'config/create_db.php';
 include 'config/create_tables.php';
-// include './create_tables.php';
-// $dbh = create_db("db_camagru");
-
-
-// echo "dbhost = ".$db_host;
-
-
 ?>
