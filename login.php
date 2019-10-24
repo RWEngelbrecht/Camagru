468829056ceb3924c0c834eafe223c2a986fe540
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-10-24 13:17:14 +0200

    added verification of login form

--- login.php
+++ login.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <?php
-include ("config/setup.php");
+// include ("config/setup.php");
+include 'includes/connect.php';
+include 'functions/functions.php';
 ini_set("display_errors", true);
 session_start();
-    // include("functions/functions.php");
 ?>
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="styles/style.css" media="all" />
+		<link rel="stylesheet" href="styles/index.css" media="all" />
 		<title>Camagru</title>
 	</head>
 	<body>
@@ -18,7 +19,7 @@ session_start();
 				</a>
 			</div>
 		<div class="main_wrapper">
-			<!--Navigation bar-->
+<!--Navigation bar-->
 			<div class="menubar">
 				<ul id="menu">
 						<li><a href="index.php">Home</a></li>
@@ -32,13 +33,13 @@ session_start();
 						</div>
 				</div>
 				<script>
-				/* When the user clicks on the button,
-				toggle between hiding and showing the dropdown content */
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
 				function myFunction() {
 				document.getElementById("myDropdown").classList.toggle("show");
 				}
 
-				// Close the dropdown if the user clicks outside of it
+// Close the dropdown if the user clicks outside of it
 				window.onclick = function(event) {
 					if (!event.target.matches('.dropbtn')) {
 						var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -53,20 +54,21 @@ session_start();
 				}
 				</script>
 			</div>
-			<!--content wrapper starts-->
+<!--content wrapper starts-->
 			<div class="content_wrapper">
-				<form method="POST">
+	<!-- login form -->
+				<form method="POST" action="login.php">
 					<table>
 						<tr align="center">
 							<td colspan="3"><h2>Please log in or register</h2></td>
 						</tr>
 						<tr align="center">
 							<td align="right">Email:</td>
-							<td><input type="text" name="email" placeholder="enter email" required/></td>
+							<td><input type="text" name="user_email" placeholder="enter email" required/></td>
 						</tr>
 						<tr>
 							<td align="right">Password:</td>
-							<td align="center"><input type="password" name="pass" placeholder="enter password" required/></td>
+							<td align="center"><input type="password" name="user_passwd" placeholder="enter password" required/></td>
 						</tr>
 						<tr align="right">
 							<td colspan="3" style="font-size:12px;padding:5px 80px"><a href="forgot_pass.php">Forgot Password?</a></td>
@@ -77,7 +79,41 @@ session_start();
 					</table>
 				</form>
 				<?php
+					if (isset($_POST['login'])) {
+	//validate email + password
+						$u_email = $_POST['user_email'];
+						$u_passwd = hash('whirlpool',$_POST['user_passwd']);
+						$get_udata = $con->prepare("SELECT * FROM users WHERE user_email=?");
+						$get_udata->execute([$u_email]);
+						$user_data = $get_udata->fetch();
 
+						if (!$user_data) {
+							echo "<script>window.alert('You don't exist yet! How about registering first?')</script>";
+						}
+		//if email from form == email from db && passwd from form == passwd from db and user has been verified
+						if ($u_email == $user_data['user_email'] && $u_passwd == $user_data['user_passwd'] && $user_data['verified'] == 1) {
+				//store session data
+							$_SESSION['user_email'] = $u_email;
+							$_SESSION['user_id'] = $user_data['user_id'];
+							$_SESSION['user_name'] = $user_data['user_name'];
+				//take client to their account page
+							echo "<script>window.open('client/my_account.php?', '_self')</script>";
+						} //if all of the above, but user has not gone through verification link emailed to them
+						else if ($u_email == $user_data['user_email'] && $u_passwd == $user_data['user_passwd'] && $user_data['verified'] == 0) {
+
+							echo "<script>window.alert('Please verify your account. We sent you instructions on how to do that.')</script>";
+							$_SESSION['user_email'] = $u_email;
+							$_SESSION['user_id'] = $user_data['user_id'];
+							$_SESSION['user_name'] = $user_data['user_name'];
+							$u_ver = $user_data['token'];
+							verif_email($u_email, $u_ver);
+						}
+						else if ($u_email == $user_data['user_email'] && $u_passwd != $user_data['user_passwd']) {
+
+							echo "<script>window.alert('Password incorrect!')</script>";
+						}
+						$con = null;    ////????
+					}
 				?>
 			</div>
 			<!--content wrapper ends-->
