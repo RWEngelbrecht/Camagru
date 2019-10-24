8c4390a0da1a560d5042364d7b08e4e980ed0f3c
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-10-24 13:09:15 +0200

    verif_usrer manually connects to db

--- functions/functions.php
+++ functions/functions.php
@@ -6,13 +6,20 @@ function verif_email($u_email, $ver_code) {
 	$headers = "From: camagru.rigardt@gmail.com\r\n";
 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 	if (mail($u_email,$subject,$body,$headers))
-	return true;
+		return true;
 	else
-	return false;
+		return false;
 }
 
 function verif_user($user_id) {
-	include ('../includes/connect.php');
+	// include ('../includes/connect.php');
+	try {
+		$con = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "qwerqwer");
+		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOException $e) {
+		echo "ERROR: ".$e->getMessage();
+	}
 	$u_verif = $con->prepare("SELECT * FROM users WHERE user_id=? AND verified=1");
 	$u_verif->execute([$user_id]);
 	$row = $u_verif->fetch();
