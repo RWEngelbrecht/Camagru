3a992ff178c5d0dd2c080b99bdb9a3413de85d1a
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-10-25 13:20:19 +0200

    more meta tags, changed menubar layout

--- index.php
+++ index.php
@@ -7,77 +7,50 @@ ini_set("display_errors", true);
 session_start();
     // include("functions/functions.php");
 ?>
-<html lang="en" dir="ltr">
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<meta name="author" content="Rigardt Engelbrecht">
 		<link rel="stylesheet" href="styles/index.css" media="all" />
 		<title>Camagru</title>
-	</head>
-	<body>
-			<div class="header_wrapper">
-				<a href="index.php">
-					<img id="banner" src="images/rengelbr_logo.png">
-				</a>
-			</div>
-		<div class="main_wrapper">
-			<!--Navigation bar-->
-			<div class="menubar">
-				<?php
-					// if (isset($_SESSION['user_id']))
-					// {
-					// 	if (verif_user($_SESSION['user_id'])) {
-					// 		echo "<ul id='my_acc_menu'>
-					// 				<li><a href='index.php'>Home</a></li>
-					// 				<li><a href='client/my_account.php?user=".$_SESSION['user_id']."'>My Account</a></li>
-					// 				<li><a href='index.php?session_status=logout'>Log Out</a></li>
-					// 				</ul>";
-					// 	}
-					// }
-					// else {
-					// 	echo "<ul id='menu'>
-					// 			<li><a href='index.php'>Home</a></li>
-					// 			</ul>
-					// 			<div class='dropdown'>
-					// 			<button onclick='myFunction()' class='dropbtn'>Login - Register</button>
-					// 			<div id='myDropdown' class='dropdown-content'>
-					// 				<a href='login.php'>Login</a>
-					// 				<a href='register.php'>Register</a>
-					// 				<a href='fml.php'>Forgot account-temp-</a>
-					// 			</div>
-					// 		</div>";
-					// }
-					get_menu();
-				?>
-				<!-- <div class="dropdown">
-						<button onclick="myFunction()" class="dropbtn">Login - Register</button>
-						<div id="myDropdown" class="dropdown-content">
-							<a href="login.php">Login</a>
-							<a href="register.php">Register</a>
-							<a href="fml.php">Forgot account-temp-</a>
-						</div>
-				</div> -->
-				<script>
-				/* When the user clicks on the button,
-				toggle between hiding and showing the dropdown content */
-				function myFunction() {
+		<script>
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+			function myFunction() {
 				document.getElementById("myDropdown").classList.toggle("show");
-				}
-
-				// Close the dropdown if the user clicks outside of it
-				window.onclick = function(event) {
-					if (!event.target.matches('.dropbtn')) {
-						var dropdowns = document.getElementsByClassName("dropdown-content");
-						var i;
-						for (i = 0; i < dropdowns.length; i++) {
-							var openDropdown = dropdowns[i];
-							if (openDropdown.classList.contains('show')) {
-								openDropdown.classList.remove('show');
-							}
+			}
+// Close the dropdown if the user clicks outside of it
+			window.onclick = function(event) {
+				if (!event.target.matches('.dropbtn')) {
+					var dropdowns = document.getElementsByClassName("dropdown-content");
+					var i;
+					for (i = 0; i < dropdowns.length; i++) {
+						var openDropdown = dropdowns[i];
+						if (openDropdown.classList.contains('show')) {
+							openDropdown.classList.remove('show');
 						}
 					}
 				}
-				</script>
+			}
+		</script>
+	</head>
+	<body>
+		<header>
+			<div class="menubar">
+				<a href="index.php">
+					<img id="banner" src="images/rengelbr_logo.png">
+				</a>
+				<div class="nav_bar">
+					<?php
+						get_menu();
+					?>
+				</div>
 			</div>
+		</header>
+		<div class="main_wrapper">
+			<!--Navigation bar-->
+
 			<!--content wrapper starts-->
 			<div class="content_wrapper">
 
@@ -85,7 +58,7 @@ session_start();
 			<!--content wrapper ends-->
 			<!--footer starts-->
 			<div id="footer">
-				<h2 style="text-align:center; padding-top:30px;">rengelbr</h2>
+				<h2 style="text-align:center; padding-top:30px;">&#169; rengelbr</h2>
 			</div>
 		</div>
 		<!--footer ends-->
