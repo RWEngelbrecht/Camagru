b19964b97c2b11ea17bba6036396096f9cf17ccf
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-10-25 13:19:30 +0200

    more meta tags, changed layout

--- client/my_account.php
+++ client/my_account.php
@@ -10,51 +10,29 @@ session_start();
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<meta name="author" content="Rigardt Engelbrecht">
 		<link rel="stylesheet" href="../styles/index.css" media="all" />
-		<title>Camagru</title>
+		<title>Camagru - My Account</title>
 	</head>
 	<body>
-			<div class="header_wrapper">
+		<header>
+			<div class="menubar">
 				<a href="index.php">
 					<img id="banner" src="../images/rengelbr_logo.png">
 				</a>
+				<div class="nav_bar">
+					<ul id="my_acc_menu">
+						<li><a href="../index.php">Home</a></li>
+						<li><a href="my_account.php?session_status=logout">Log Out</a></li>
+					</ul>
+				</div>
 			</div>
+		</header>
 		<div class="main_wrapper">
 			<!--Navigation bar-->
 			<div class="menubar">
-				<ul id="my_acc_menu">
-						<li><a href="../index.php">Home</a></li>
-						<li><a href="my_account.php?session_status=logout">Log Out</a></li>
-				</ul>
-				<!-- <div class="dropdown">
-						<button onclick="myFunction()" class="dropbtn">Login - Register</button>
-						<div id="myDropdown" class="dropdown-content">
-							<a href="../login.php">Login</a>
-							<a href="../register.php">Register</a>
-							<a href="../fml.php">Forgot account-temp-</a>
-						</div>
-				</div> -->
-				<!-- <script>
-				/* When the user clicks on the button,
-				toggle between hiding and showing the dropdown content */
-				function myFunction() {
-					document.getElementById("myDropdown").classList.toggle("show");
-				}
 
-				// Close the dropdown if the user clicks outside of it
-				window.onclick = function(event) {
-					if (!event.target.matches('.dropbtn')) {
-						var dropdowns = document.getElementsByClassName("dropdown-content");
-						var i;
-						for (i = 0; i < dropdowns.length; i++) {
-							var openDropdown = dropdowns[i];
-							if (openDropdown.classList.contains('show')) {
-								openDropdown.classList.remove('show');
-							}
-						}
-					}
-				}
-				</script> -->
 			</div>
 			<!--content wrapper starts-->
 			<div class="content_wrapper">
@@ -73,7 +51,7 @@ session_start();
 			<!--content wrapper ends-->
 			<!--footer starts-->
 			<div id="footer">
-				<h2 style="text-align:center; padding-top:30px;">rengelbr</h2>
+				<h2 style="text-align:center; padding-top:30px;">&#169; rengelbr</h2>
 			</div>
 		</div>
 		<!--footer ends-->
