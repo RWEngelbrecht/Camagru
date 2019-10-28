f8536e0323bebe41c8c76c1a6625148f7fe9b31a
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-10-28 18:05:41 +0200

    added images table to db

--- config/create_tables.php
+++ config/create_tables.php
@@ -1,13 +1,11 @@
 <?php
 include "config/database.php";
 
-$table = "users";
-
 try {
 	$con = new PDO($dsn.";dbname=".$db_name, $db_user, $db_passwd);
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	$sql = "CREATE TABLE IF NOT EXISTS $table(
+	$usr = "CREATE TABLE IF NOT EXISTS users(
 	`user_id` INT(100) AUTO_INCREMENT PRIMARY KEY,
 	`user_name` VARCHAR(255) NOT NULL,
 	`user_passwd` VARCHAR(255) NOT NULL,
@@ -17,7 +15,14 @@ try {
 	`token` VARCHAR(255) NOT NULL,
 	`verified` BIT default 0 NOT NULL)";
 
-	$con->exec($sql);
+	$con->exec($usr);
+
+	$img = "CREATE TABLE IF NOT EXISTS images(
+		`u_id` INT(100) PRIMARY KEY,
+		`image` LONGBLOB,
+		`date_created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+
+	$con->exec($img);
 }
 catch(PDOException $e) {
 	echo "ERROR: ".$e->getMessage();
