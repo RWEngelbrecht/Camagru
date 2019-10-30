cf1d569b8ef1399e691577e3f6676556058af288
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-10-30 13:33:03 +0200

    new navbar, update account, better log out

--- client/my_account.php
+++ client/my_account.php
@@ -10,48 +10,55 @@ session_start();
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="author" content="Rigardt Engelbrecht">
-		<link rel="stylesheet" href="../styles/index.css" media="all" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 		<title>Camagru - My Account</title>
+
 	</head>
 	<body>
 		<header>
-			<a href="../index.php">
+			<!-- <a href="../index.php">
 				<img id="banner" src="../images/rengelbr_logo.png">
 			</a>
 			<div class="menubar">
-			<!--Navigation bar-->
 				<div class="nav_bar">
 					<ul id="my_acc_menu">
 						<li><a href="../index.php">Home</a></li>
-						<li><a href='new_upload.php'>New Upload</a></li>
+						<li><a href='../new_upload.php'>New Upload</a></li>
 						<li><a href="my_account.php?session_status=logout">Log Out</a></li>
 					</ul>
 				</div>
+			</div> -->
+			<div class="navbar">
+			<div class="navbar-brand">
+				<a href="index.php">
+					<img id="banner" src="../images/rengelbr_logo.png">
+				</a>
+				<div class='navbar-start'>
+					<div class='navbar-item'>
+						<a class='button is-primary' href='../index.php'>Home</a>
+					</div>
+					<div class='navbar-item'>
+						<a class='button is-light' href='../new_upload.php'>New Upload</a>
+					</div>
+					<div class='navbar-item'>
+						<a class='button is-light' href='my_account.php?session_status=logout'>Log Out</a>
+					</div>
+				</div>
 			</div>
 		</header>
-		<div class="main_wrapper">
+		<section class="section">
 <!--content wrapper starts-->
-			<div class="content_wrapper">
-				<?php
-					// if (isset($_SESSION['user_id'])) {
-					// 	if (verif_user($_SESSION['user_id']))
-					// 		echo "<h2 >Welcome, ".$_SESSION['user_name']."</h2>";
-					// 	else
-					// 		echo "<h2>You're not supposed to be here!</h2>";
-					// }
-					// else {
-					// 	echo "<h2>Welcome, whoever you are! Please log in or register.</h2>";
-					// }
-				?>
-				<div id="account_tools">
+			<div class="container">
+				<!-- <div id="account_tools">
 					<?php
 						$get_udata = $con->prepare("SELECT * FROM users WHERE user_email=?");
 						$get_udata->execute([$_SESSION['user_email']]);
 						$u_data = $get_udata->fetch();
 						$u_img = $u_data['user_image'];
-						echo "<img src='data:image/png;base64,".$u_img."' />";
+						// echo "<img src='data:image/png;base64,".$u_img."' />";
 					?>
 					<ul>
 						<li>
@@ -61,25 +68,38 @@ session_start();
 				</div>
 				<div id="account_gallery">
 						<h2>Cage Time!</h2>
-					<?php
-						if (isset($_GET['session_status'])){include 'update_account.php';}
-						if (isset($_GET['update'])){
-							update_user($u_data['user_id']);
-						}
-					?>
-				</div>
-			</div>
+				</div> -->
+				<aside class="menu" style="float:left">
+					<p class="menu-label">
+						<?php echo $_SESSION['user_name'] ?>
+					</p>
+					<ul class="menu-list">
+						<li><a id="open-modal" href="my_account.php?session_status=update">Edit Account</a></li>
+					</ul>
+				</aside>
+		</section>
+		<section class="section">
+			<?php
+				if (isset($_GET['session_status'])){
+					include_once 'update_account.php';
+				}
+				if (isset($_POST['updt_name']) || isset($_POST['updt_email']) || isset($_POST['updt_passwd']) || isset($_POST['updt_image'])){
+					update_user($u_data['user_id']);
+				}
+			?>
+		</section>
 <!--content wrapper ends-->
 <!--footer starts-->
 			<div id="footer">
 				<h2 style="text-align:center; padding-top:30px;">&#169; rengelbr</h2>
 			</div>
-		</div>
 		<!--footer ends-->
 	</body>
 </html>
 <?php
 if (isset($_GET['session_status'])) {
-	log_out("my_account");
+	if ($_GET['session_status'] == "logout") {
+		log_out("my_account");
+	}
 }
 ?>
