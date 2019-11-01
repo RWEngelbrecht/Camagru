a6f93aabf06ba764143e23b4a91f72ad2ce16eca
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-11-01 08:02:55 +0200

    added columns, can upload images from this page

--- new_upload.php
+++ new_upload.php
@@ -8,76 +8,59 @@ session_start();
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="author" content="Rigardt Engelbrecht">
-		<link rel="stylesheet" href="styles/index.css" media="all" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 		<title>Camagru</title>
-		<script>
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-			function myFunction() {
-				document.getElementById("myDropdown").classList.toggle("show");
-			}
-// Close the dropdown if the user clicks outside of it
-			window.onclick = function(event) {
-				if (!event.target.matches('.dropbtn')) {
-					var dropdowns = document.getElementsByClassName("dropdown-content");
-					var i;
-					for (i = 0; i < dropdowns.length; i++) {
-						var openDropdown = dropdowns[i];
-						if (openDropdown.classList.contains('show')) {
-							openDropdown.classList.remove('show');
-						}
-					}
-				}
-			}
-		</script>
 	</head>
 	<body>
 		<header>
-			<div class="menubar">
-				<a href="index.php">
-					<img id="banner" src="images/rengelbr_logo.png">
-				</a>
-				<div class="nav_bar">
+			<div class="navbar">
+				<div class="navbar-brand">
+					<a href="index.php">
+						<img id="banner" src="images/CAMAGRUFORU.png">
+					</a>
 					<?php
 						get_menu();
 					?>
 				</div>
 			</div>
 		</header>
-		<div class="main_wrapper">
-			<!--content wrapper starts-->
-			<div class="content_wrapper">
-				<div class="upload_wrapper">
-					<div id="upload_main">
-						<!-- Should show webcam, webcam button, upload img button-->
-						<h1>upload main</h1>
-						<form action="" enctype="multipart/form-data" method="POST">
-							<input name="upl_image" type="file">
-							<input name="upload" type="submit" value="Upload Picture">
-							<?php if (isset($_POST['upload'])){
-									// if (isset($_FILE['upl_img'])){
-										upload_image($_SESSION['user_id']);
-									// }
-								} ?>
-						</form>
+		<main>
+			<section class="section">
+				<div class="columns is-vcentered">
+	<!-- Should show webcam, webcam button, upload img button-->
+					<div class="column" style="background:blue">
+						<div class="container">
+							<form action="" enctype="multipart/form-data" method="POST">
+								<input name="upl_image" type="file">
+								<input name="upload" type="submit" value="Upload Picture">
+								<?php
+									if (isset($_POST['upload'])){
+										if (isset($_FILES['upl_image']['name'])){
+											upload_image($_SESSION['user_id']);
+										}
+									}
+								?>
+							</form>
+						</div>
 					</div>
-					<div id="upload_side">
+					<div class="column is-one-quarter" style="background:black">
 						<!-- Display previous pics taken/uploaded -->
-						<h1>upload side</h1>
-						<?php //get_upload_thumbs(); ?>
+						<div class="container">
+							<?php //get_upload_thumbs($_SESSION['user_id']); ?>
+						</div>
 					</div>
 				</div>
-			</div>
-			<!--content wrapper ends-->
-			<!--footer starts-->
-			<div id="footer">
-				<h2 style="text-align:center; padding-top:30px;">&#169; rengelbr</h2>
-			</div>
-		</div>
-		<!--footer ends-->
+			</section>
+		<main>
 	</body>
+	<footer>
+		<div id="footer">
+			<h2 style="text-align:center; padding-top:30px;">&#169; rengelbr</h2>
+		</div>
+	</footer>
 </html>
 <?php
 if (isset($_GET['session_status'])) {
