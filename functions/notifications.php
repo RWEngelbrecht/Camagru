a66f1296e2ee2f954b39fa14c35b13eb79849b24
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-11-05 19:41:13 +0200

    checks users notification prefs before emailing

--- functions/notifications.php
+++ functions/notifications.php
@@ -4,19 +4,18 @@ function notify_comment($user) {
     include 'includes/connect.php';
 
     if (verif_user($user)) {
-        $usr_email_sql = "SELECT user_email FROM users WHERE user_id=:usr_id";
+        $usr_email_sql = "SELECT * FROM users WHERE user_id=:usr_id";
         $get_usr_email = $con->prepare($usr_email_sql);
         $get_usr_email->execute([':usr_id'=>$user]);
         $usr_email = $get_usr_email->fetch();
 
-        $subject = "Camagru Comments";
-        $body = "Hey! Someone commented on one of your posts.";
-        $headers = "From: camagru.rigardt@gmail.com\r\n";
-        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-        if (mail($usr_email['user_email'],$subject,$body,$headers))
-            return true;
-        else
-            return false;
+		if ($usr_email['notify'] == 1) {
+			$subject = "Camagru Comments";
+			$body = "Hey! Someone commented on one of your posts.";
+			$headers = "From: camagru.rigardt@gmail.com\r\n";
+			$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+			mail($usr_email['user_email'],$subject,$body,$headers);
+		}
     }
 
 }
