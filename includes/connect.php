eb0c7da8c717199ea88eca792321f1be0940b65a
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-11-05 19:41:37 +0200

    remember to change passwd

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$con = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "qwerqwer");
+	$con = new PDO("mysql:host=localhost;dbname=db_camagru", "root", "root");
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
