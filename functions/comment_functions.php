16271cc1f95092ba34d52903379d8ca0b4c84408
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-11-05 14:43:35 +0200

    added notify_comment function

--- functions/comment_functions.php
+++ functions/comment_functions.php
@@ -54,18 +54,23 @@ function get_comments($img_id) {
 
 function post_comment($img) {
 	include 'includes/connect.php';
+	include 'functions/notifications.php';
+	include_once 'functions/functions.php';
 
 	if (isset($_SESSION['user_id'])) {
 		$comment = $_POST['cmntContent'];
 		$commentor_id = $_SESSION['user_id'];
-	}
-	if ($commentor_id) {
 
-		$post_cmnt_sql = "INSERT INTO comments(cmnt_img_id, cmnt_usr_id, comment) VALUES(:img_id, :cmntr_id, :cmnt)";
-		$post_cmnt = $con->prepare($post_cmnt_sql);
-		$post_cmnt->execute(array(':img_id'=>$img, ':cmntr_id'=>$commentor_id, ':cmnt'=>$comment));
-	} else {
-		echo "<script>alert('Please Log In or Register to like or comment!')</script>";
+		if ($commentor_id) {
+
+			$post_cmnt_sql = "INSERT INTO comments(cmnt_img_id, cmnt_usr_id, comment) VALUES(:img_id, :cmntr_id, :cmnt)";
+			$post_cmnt = $con->prepare($post_cmnt_sql);
+			$post_cmnt->execute(array(':img_id'=>$img, ':cmntr_id'=>$commentor_id, ':cmnt'=>$comment));
+			$op = get_post_user($img);
+			notify_comment($op);
+		} else {
+			echo "<script>alert('Please Log In or Register to like or comment!')</script>";
+		}
 	}
 }
 
