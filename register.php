6ed12d86e630cb11b419406da5fd1d2c7e5dc0b0
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-11-06 07:51:25 +0200

    fixed footer, extra stylesheet

--- register.php
+++ register.php
@@ -12,20 +12,7 @@ session_start();
 		<meta name="author" content="Rigardt Engelbrecht">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
 		<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-		<style>
-			.navbar-brand {
-				position: fixed;
-				top: 0;
-				left: 0;
-				width: 100%;
-				height: 220px;
-				z-index: 10;
-				background: #eeeeee;
-				-webkit-box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
-				-moz-box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
-				box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
-			}
-		</style>
+		<link rel="stylesheet" href="http://localhost:8080/Camagru/styles/index.css">
 		<title>Camagru - Register</title>
 	</head>
 	<body>
@@ -41,7 +28,7 @@ session_start();
 				</div>
 			</div>
 		</header>
-		<section class="section" style="margin-top:150px">
+		<section class="section" style="margin-top:150px;margin-bottom:100px">
 			<div class="container">
 				<p class="title">Ready To Become a More Complete Person?</p>
 				<form action="register.php" method="POST" enctype="multipart/form-data">
@@ -88,14 +75,14 @@ session_start();
 				</form>
 			</div>
 		</section>
-<!--content wrapper ends-->
-<!--footer starts-->
-			<div id="footer">
-				<h2 style="text-align:center; padding-top:30px;">&#169; rengelbr</h2>
-			</div>
-		</div>
-<!--footer ends-->
 	</body>
+<!--footer starts-->
+	<footer>
+		<div id="footer">
+			<h2 style="text-align:center; padding-top:30px;">&#169; rengelbr</h2>
+		</div>
+	</footer>
+<!--footer ends-->
 </html>
 <?php
 //check if register button clicked
