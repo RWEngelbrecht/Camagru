90c1a84ed1f635abdab4dc351d51ad61553e05c3
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-11-06 07:46:13 +0200

    corrected alignment of notification checkbox

--- client/update_account.php
+++ client/update_account.php
@@ -57,13 +57,15 @@
 <form method="POST">
 	<div class="level">
 		<div class="level-left">
-			<label class='label'>Notifications: </label>
-		</div>
-		<div class="level-item">
-			<input type="checkbox" name="notif" <?php if($_SESSION['notif'] == 1){echo "checked";} ?>>
-		<!-- </div> -->
-		<!-- <div class="level-item" > -->
-		<input class="button is-success is-medium" type="submit" name="updt_notif" value="Update">
+			<div class="level-item">
+				<label class='label'>Notifications: </label>
+			</div>
+			<div class="level-item">
+				<input type="checkbox" name="notif" <?php if($_SESSION['notif'] == 1){echo "checked";} ?>>
+			</div>
+			<div class="level-item">
+				<input class="button is-success is-medium" type="submit" name="updt_notif" value="Update">
+			</div>
 		</div>
 	</div>
 </form>
