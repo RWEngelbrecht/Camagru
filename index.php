9c4eed24f5467856663ec67c6fbea9c9382414d9
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-11-06 07:49:37 +0200

    added fixed footer, pseudo-infinite scrolling, extra stylesheet

--- index.php
+++ index.php
@@ -11,26 +11,12 @@ session_start();
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="author" content="Rigardt Engelbrecht">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		<link rel="stylesheet" href="http://localhost:8080/Camagru/styles/index.css">
 		<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 		<title>Camagru</title>
-		<style>
-			.navbar-brand {
-				position: fixed;
-				top: 0;
-				left: 0;
-				width: 100%;
-				height: 220px;
-				z-index: 10;
-				background: #eeeeee;
-				-webkit-box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
-				-moz-box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
-				box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
-			}
-		</style>
 	</head>
 	<body>
 		<header>
-			<!-- <div class="navbar"> -->
 				<div class="navbar-brand">
 					<a href="index.php">
 						<img id="banner" src="images/CAMAGRUFORU.png">
@@ -39,16 +25,13 @@ session_start();
 						get_menu();
 					?>
 				</div>
-			<!-- </div> -->
 		</header>
-		<main >
-			<section class="section" style="margin-top:190px">
+		<main>
+			<section class="section" style="margin-top:190px;margin-bottom:100px">
 				<div class="container is-fluid">
-					<!-- <div class="gallery"> -->
-						<?php
-							get_gallery();
-						?>
-					<!-- </div> -->
+					<?php
+						get_gallery();
+					?>
 				</div>
 			</section>
 		</main>
